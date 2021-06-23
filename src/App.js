fab53dce4e5d0954a7a38adb2dd0942bef4c029c
Nguyen Trieu <nguyentrieussj4@gmail.com>
2021-06-23 11:06:49 +0700

    useLayoutEffect awesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import React, {
-  useState,
-  useEffect,
-  useRef,
-  createContext,
-  useMemo,
-} from "react";
+import React, { useState, useEffect, useRef, createContext } from "react";
 import Toggle from "./Toggle";
 import Counter from "./Counter";
 import { useTitleInput } from "./hooks/useTitleInput";
@@ -14,16 +8,32 @@ export const UserContext = createContext();
 const App = () => {
   const [name, setName] = useTitleInput("");
   const ref = useRef();
-  // console.log("ref", ref);
 
-  const reverseWord = (word) => {
-    console.log("function called");
-    return word.split("").reverse().join("");
+  const [dishes, setDishes] = useState([]);
+
+  const fetchDishes = async () => {
+    console.log("ran");
+    const res = await fetch(
+      `https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes`
+    );
+    const data = await res.json();
+    setDishes(data);
   };
 
-  const title = "LOVE LOVE LOVE EVIL";
+  useEffect(() => {
+    fetchDishes();
+  }, [name]);
 
-  const TitleReversed = useMemo(() => reverseWord(title), [title]);
+  // console.log("ref", ref);
+
+  // const reverseWord = (word) => {
+  //   console.log("function called");
+  //   return word.split("").reverse().join("");
+  // };
+
+  // const title = "LOVE LOVE LOVE EVIL";
+
+  // const TitleReversed = useMemo(() => reverseWord(title), [title]);
 
   return (
     <UserContext.Provider
@@ -33,7 +43,7 @@ const App = () => {
     >
       <div className="main-wrapper" ref={ref}>
         <h1 onClick={() => ref.current.classList.add("new-fake-class")}>
-          {TitleReversed}
+          Level Up Dishes
         </h1>
         <Toggle />
         {/* <Counter /> */}
@@ -49,6 +59,17 @@ const App = () => {
           />
           <button>Submit</button>
         </form>
+        {dishes.map((dish) => (
+          <article className="dish-card dish-card--withImage">
+            <h3>{dish.name}</h3>
+            <p>{dish.desc}</p>
+            <div className="ingredients">
+              {dish.ingredients.map((ingredient) => (
+                <span>{ingredient}</span>
+              ))}
+            </div>
+          </article>
+        ))}
       </div>
     </UserContext.Provider>
   );
